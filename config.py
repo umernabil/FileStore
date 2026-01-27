0180dfc337356f06401522aabd8d76337de93055
umernabil <umernabil057@gmail.com>
2026-01-27 08:50:54 +0300

    Update database channel configuration in config.py

--- config.py
+++ config.py
@@ -24,11 +24,11 @@ DB_NAME = "filestore"
 
 FSUBS = [[-1002201035608, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL =    # just put channel id dont add "-1003699328654"
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
-#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
-#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+#     "-1003699328654": {"name": "Primary DB", "is_primary": True, "is_active": True},
+#     "--1003699328654": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
