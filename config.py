1be9dd3ac87f92e0d0960959306fd9d1a70f4ccb
umernabil <umernabil057@gmail.com>
2026-02-11 18:29:15 +0300

    Clear force subscription channels in config.py

--- config.py
+++ config.py
@@ -22,8 +22,7 @@ WORKERS = 5
 DB_URI = "mongodb+srv://Omars:12345678omar@cluster5.yfqfy7o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster5"
 DB_NAME = "filestore"
 
-FSUBS = [[-1002362485325, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
-# Database Channel (Primary)
+FSUBS = FSUBS = []  # No force subscription channels
 DB_CHANNEL =   "1001760325699"
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
